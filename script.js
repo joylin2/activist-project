c392432e76918d7eb51a148cb545eaf15b0aa63c
Glitch (yasmin-joy-activist) <none>
2020-07-30 22:11:27 +0000

    👨‍👨‍👦👬 Updated with Glitch

---
+++ script.js
@@ -0,0 +1,16 @@
+/************************************************/
+/********* ADD CHECKLIST FUNCTIONALITY **********/
+/************************************************/
+                                         
+var fullItemList = document.getElementById("my-list");
+
+if (fullItemList) {
+  fullItemList.addEventListener("click", checkOffItem);
+}
+
+function checkOffItem(clicked) {
+    if (clicked.target.tagName == "LI") {
+        clicked.target.classList.toggle("all-done");
+    }
+}
+
